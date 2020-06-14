e8d1307f4a8c664c8284621f76f7ae1f4caf774f
amirsuhail <40598026+amirsuh@users.noreply.github.com>
2020-06-14 11:21:35 +0530

    Update flentos_test.js

--- flentos_test.js
+++ flentos_test.js
@@ -1,35 +1,36 @@
+// Function to return the minimum cost 
 function findCost(cost, n) {
+    // Sort the price array 
+    cost = cost.sort()
+    fullCost = 0
 
-     cost = cost.sort() 
-     fullCost = 0
-  
-  for (let i = n - 1; i > 1; i -= 2){
-  
-            if (i == 2) 
-            { 
-                fullCost += cost[2] + cost[0]; 
-            } 
-            else
-            { 
-      
-                // Both the ways as discussed above 
-                 let price_first = cost[i] + cost[0] + 2 * cost[1]; 
-                 let price_second = cost[i] + cost[i - 1] + 2 * cost[0]; 
-                fullCost += Math.min(price_first, price_second); 
-            } 
+    //Calcualte minimum price 
+    //of n-2 most costly person 
+    for (let i = n - 1; i > 1; i -= 2) {
 
-  }
-        if (n == 1) 
-        { 
-            fullCost += cost[0]; 
-        } 
-        else
-        { 
-            fullCost += cost[1]; 
-        } 
-      
-        return fullCost;
+        if (i == 2) {
+            fullCost += cost[2] + cost[0];
+        }
+        else {
+
+            // Both the ways as discussed above 
+            let price_first = cost[i] + cost[0] + 2 * cost[1];
+            let price_second = cost[i] + cost[i - 1] + 2 * cost[0];
+            fullCost += Math.min(price_first, price_second);
+        }
+
+    }
+    //Calculate the minimum price 
+    //of the two cheapest person 
+    if (n == 1) {
+        fullCost += cost[0];
+    }
+    else {
+        fullCost += cost[1];
+    }
+
+    return fullCost;
 }
-cost = [300,400,600,700] 
-n = cost.length 
-findCost(cost,n)
+cost = [300, 400, 600, 700]
+n = cost.length
+findCost(cost, n)
